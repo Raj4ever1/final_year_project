7e0fdc951073f644681570d9cb590467ce7f5a0f
Ronak Jain <rj.raj4ever@gmail.com>
2020-12-20 11:59:26 +0545

    done

---
+++ register/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from register import views
+urlpatterns = [
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('login/login',views.login),
+    path('signup/signup',views.signup),
+    path('activate/<uidb64>/<token>',views.activate,name='activate')
+]
---
+++ try.py
@@ -0,0 +1,9 @@
+import sqlite3
+con=sqlite3.connect('db.sqlite3')
+work=()
+for i in con.execute('SELECT name FROM sqlite_master WHERE type="table";'):
+    print(i[0],con.execute(f'pragma table_info({i[0]})').fetchall())
+for i in con.execute('select * from auth_user;'):print(i)
+con.execute('delete from auth_user;')
+con.commit()
+for i in con.execute('select * from auth_user;'):print(i)

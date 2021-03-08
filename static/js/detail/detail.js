8d33819cc6907e73a2e7301e9c61c834322f841c
MSaiJagapathi <msaij@users.noreply.github.com>
2021-03-08 22:47:18 +0530

    .

--- static/js/detail/detail.js
+++ static/js/detail/detail.js
@@ -3,7 +3,7 @@ for (var i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.classList === "active") {
+    if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
     } else {
       dropdownContent.style.display = "block";
